178eed5addf718c87f6b482d0533444e541bd7b6
Facundo Spira <facundospira@gmail.com>
2023-10-29 13:03:46 -0300

    feat: add home endpoint to fetch to p movies and series

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :ratings, only: [:create, :update]
   resource :recommendations, only: [:show]
   resources :favourites, only: [:create, :index, :destroy]
+  root to: 'home#index'
 end
