43d99449674408679fba0071f71a0a861701fb6d
Facundo Spira <facundospira@gmail.com>
2023-10-29 14:15:12 -0300

    feat: add user delete method

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:create]
+  resources :users, only: [:create, :destroy]
   resources :ratings, only: [:create, :update]
   resource :recommendations, only: [:show]
   resources :favourites, only: [:create, :index, :destroy]
