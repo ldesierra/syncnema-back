622e1f69d1fb3c140ad4a89e320d1f0f14da2999
Facundo Spira <facundospira@gmail.com>
2023-10-29 13:59:18 -0300

    feat: improve recommendations endoint

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -2,10 +2,17 @@ class RecommendationsController < ApplicationController
   def show
     user = User.find_by(external_id: params[:user_id])
 
-    return render json: { error: 'User not found' }, status: 404 if user.blank?
+    render json: get_generic_recommendations, status: 200 and return if user.blank?
 
     recommendations = Recommender.call(user)
+    recommendations = get_generic_recommendations if recommendations.blank?
 
     render json: recommendations, status: 200
   end
+
+  private
+  def get_generic_recommendations
+    Content.order(rating: :desc).limit(10)
+  end
+
 end
