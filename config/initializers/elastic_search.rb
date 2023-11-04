4f04409dafe42f1a7bbbae98a805e3777e7b1e5d
Martin Larroca <martinlarroca@192.168.1.76>
2023-11-03 22:37:02 -0300

    feat: do not verify ssl in elasticsearch connection

--- config/initializers/elastic_search.rb
+++ config/initializers/elastic_search.rb
@@ -1,1 +1,1 @@
-Elasticsearch::Model.client = Elasticsearch::Client.new(url: 'https://docker3649-env-8954386.web.elasticloud.uy:9200')
+Elasticsearch::Model.client = Elasticsearch::Client.new(url: 'https://docker3649-env-8954386.web.elasticloud.uy:9200', ssl: { verify: false })
