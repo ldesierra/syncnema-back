774d2fbc19b2b2349a23dc3e9b69153dd7fd55c6
Facundo Spira <facundospira@gmail.com>
2023-11-10 21:48:20 -0300

    chore: remove trailing slash

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://syncnema.vercel.app',
-            'https://syncnema-git-sa-27-flocarle.vercel.app/',
+            'https://syncnema-git-sa-27-flocarle.vercel.app',
             'http://localhost:3000'
 
     resource '*',
