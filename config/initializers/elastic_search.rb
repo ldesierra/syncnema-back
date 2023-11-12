6185778da2762d76fe82ce2356c15c843fd29407
Facundo Spira <facundospira@gmail.com>
2023-11-11 23:04:22 -0300

    chore: revert changes

--- config/initializers/elastic_search.rb
+++ config/initializers/elastic_search.rb
@@ -1,9 +1,13 @@
-settings = { url: 'https://elastic:qGd_hgPzRSUmnyn0qAbq@179.31.2.183:9200',
+settings = if Rails.env.development?
+             { url: 'http://localhost:9200' }
+           else
+             { url: 'https://elastic:qGd_hgPzRSUmnyn0qAbq@179.31.2.183:9200',
                transport_options: {
                  ssl: {
                    verify: false
                  }
                 }
               }
+           end
 
 Elasticsearch::Model.client = Elasticsearch::Client.new(settings)
