15c9da72cd075c5a365c1cd8d92ce9f3872439c0
ldesierra <83076120+ldesierra@users.noreply.github.com>
2023-11-11 21:10:30 -0300

    Fixes for prod

--- config/initializers/elastic_search.rb
+++ config/initializers/elastic_search.rb
@@ -1,8 +1,13 @@
-Elasticsearch::Model.client = Elasticsearch::Client.new(
-  url: 'https://elastic:qGd_hgPzRSUmnyn0qAbq@179.31.2.183:9200',
-  transport_options: {
-    ssl: {
-      verify: false
-    }
-  }
-)
+settings = if Rails.env.development?
+             { url: 'http://localhost:9200' }
+           else
+             { url: 'https://elastic:qGd_hgPzRSUmnyn0qAbq@179.31.2.183:9200',
+               transport_options: {
+                 ssl: {
+                   verify: false
+                 }
+                }
+              }
+           end
+
+Elasticsearch::Model.client = Elasticsearch::Client.new(settings)
